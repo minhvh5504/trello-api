43fccb64cc78517f654921f661c18508a9109236
Minh <vihongminh5504@gmail.com>
2024-03-12 02:08:18 +0700

    init code base

--- src/server.js
+++ src/server.js
@@ -14,19 +14,20 @@ const port = 8017
 
 app.get('/', (req, res) => {
   // Test Absolute import mapOrder
+  // eslint-disable-next-line no-console
   console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
+    [{ id: 'id-1', name: 'One' },
       { id: 'id-2', name: 'Two' },
       { id: 'id-3', name: 'Three' },
       { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
+      { id: 'id-5', name: 'Five' }],
     ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
     'id'
   ))
-  res.end('<h1>Hello World!</h1><hr>')
+  res.end('<h1>Chào mừng trở lại</h1><hr>')
 })
 
 app.listen(port, hostname, () => {
   // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at http://${ hostname }:${ port }/`)
+  console.log(`I am running at http://${ hostname }:${ port }/`)
 })
