8c8f0745dece060a30ec664437d80eb9c3b06bdc
Minh <vihongminh5504@gmail.com>
2024-03-11 23:29:19 +0700

    first commit

--- src/server.js
+++ src/server.js
@@ -28,5 +28,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, hostname, () => {
   // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
+  console.log(`Hello Trung Quan Dev, I am running at http://${ hostname }:${ port }/`)
 })
